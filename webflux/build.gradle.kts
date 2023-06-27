1a5325133a04d945f862712d9605dd02b6e72d4c
Alexey Morozov <morozov@ac-sw.com>
2023-06-27 12:57:34 +0700

    removed excessive dependency

--- webflux/build.gradle.kts
+++ webflux/build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
